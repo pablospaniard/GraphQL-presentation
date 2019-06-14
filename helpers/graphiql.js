47eb423e3809fc0a114fe7fe3664bbfd72975d10
Pavel Ivanov <pablospaniard@gmail.com>
2019-06-14 17:18:19 +0200

    fetching wip

--- helpers/graphiql.js
+++
@@ -1,9 +0,0 @@
-export const graphQLFetcher = async graphQLParams => {
-    return fetch('https://metaphysics-production.artsy.net/', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(graphQLParams)
-    }).then(response => response.json())
-}
